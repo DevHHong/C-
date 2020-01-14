8943de5b863dc83b1c6317784e84986e465bd297
hongju8809 <hongju940809@gmail.com>
2020-01-14 21:02:47 +0900

    음 구구단 씨샵으로 한번 만들어봣

---
+++ 9x9.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 9; ++i)
+            {
+                Console.WriteLine("{0}단\n", i);
+                for (int j = 1; j <= 9; j++)
+                {
+                    Console.WriteLine("{0} * {1} = {2}", i, j, i * j);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
